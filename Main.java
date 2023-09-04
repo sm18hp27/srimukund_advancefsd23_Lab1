1702bf887d38205ba38f97cec6b01958b76e9158
sm18hp27 <144050576+sm18hp27@users.noreply.github.com>
2023-09-04 22:28:57 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+package employeemodel;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Employee newEmployee = new Employee("hari", "mukund");
+	        newEmployee.setDepartment("Developer");
+
+	        credencialService.showCredentials(newEmployee);
+	    }
+	
+
+	}
+
+
